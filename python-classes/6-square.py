5e6018afeaa3e02f71a315eadefe36bb8b0ff1fe
atanih <torresalva09@gmail.com>
2026-05-30 17:09:39 -0400

    Update missing _ in self position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,4 +50,4 @@ class Square:
         for _ in range(self.__position[1]):
             print("")
         for _ in range(self.__size):
-            print(" " * self._position[0] + "#" * self._size)
+            print(" " * self.__position[0] + "#" * self.__size)
