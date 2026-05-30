787759171a130cbf71e88dc2c46ec626f0d65e04
atanih <torresalva09@gmail.com>
2026-05-30 17:45:03 -0400

    Update

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,8 +5,8 @@
 class Square:
     """This class defines a square."""
 
-    def _init_(self, size=0, position=(0, 0)):
-        """Initialize a new Square instance with size and position."""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new Square instance with an optional size and position."""
         self.size = size
         self.position = position
 
@@ -31,13 +31,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """Set the position with type and value validation."""
-        if (not isinstance(value, tuple) or len(value) != 2
-                or not isinstance(value[0], int)
-                or not isinstance(value[1], int)
-                or value[0] < 0 or value[1] < 0):
-            raise TypeError(
-                "position must be a tuple of 2 positive integers")
+        """Set the position with validation."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not isinstance(value[0], int) or not isinstance(value[1], int) or
+                value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -45,7 +43,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Print the square with the character #."""
+        """Print the square using # with position offset."""
         if self.__size == 0:
             print("")
             return
