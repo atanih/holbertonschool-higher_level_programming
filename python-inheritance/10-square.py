4d6db10924762ba38b4dc2ad2911863a28b33bc5
atanih <torresalva09@gmail.com>
2026-05-31 19:42:39 -0400

    Push task error

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square class that inherits from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a new Square with size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return a string description of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
