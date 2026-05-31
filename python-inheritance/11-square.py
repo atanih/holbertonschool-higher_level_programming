73cbbaf2aff74e6d0e64faf1f845477f9e2d0fed
atanih <torresalva09@gmail.com>
2026-05-31 19:52:59 -0400

    Update __

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a Square class."""
-Rectangle = _import_('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
