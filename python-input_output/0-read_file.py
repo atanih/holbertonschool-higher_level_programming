f6082eca0417c8321a07a85ff2d0e3041abd6ce8
atanih <torresalva09@gmail.com>
2026-06-07 22:46:09 -0400

    Fix Error

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module for reading a text file."""
+
+
 def read_file(filename=""):
     """Reads a text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
--- python-input_output/README.md
+++ python-input_output/README.md
@@ -1,1 +1,1 @@
-This is a Repo README 
+This is a Repo README
