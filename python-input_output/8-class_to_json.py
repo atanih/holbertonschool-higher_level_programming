96bb9a539c34e436fbb218d2530db6e0fc12122a
atanih <torresalva09@gmail.com>
2026-06-07 23:39:00 -0400

    Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for returning the dictionary description of an object for JSON serialization"""
+
+
+def class_to_json(obj):
+    """Return the dictionary description with simple data structure for JSON serialization.
+
+    Args:
+        obj: An instance of a Class
+
+    Returns:
+        A dictionary representation of the object
+    """
+    return obj._dict_
