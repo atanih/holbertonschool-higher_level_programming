38c01c7939f0395736f0285bcb9ad12ace6cf337
atanih <torresalva09@gmail.com>
2026-06-07 23:35:45 -0400

    Fix typo

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Script that adds all arguments to a Python list and saves them to a file"""
 import sys
-save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
-load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
