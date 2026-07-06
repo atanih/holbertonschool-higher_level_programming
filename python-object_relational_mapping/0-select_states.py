c1f9b8faff1c52643f9c16f224f3cb70f858fbb4
atanih <torresalva09@gmail.com>
2026-07-05 20:34:30 -0400

    White spaces delete

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,20 +5,19 @@ import sys
 
 
 if __name__ == "__main__":
-    """Connect to MySQL and print all states ordered by id."""
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
-
+"""Connect to MySQL and print all states ordered by id."""
+db = MySQLdb.connect(
+host="localhost",
+port=3306,
+user=sys.argv[1],
+passwd=sys.argv[2],
+db=sys.argv[3],
+charset="utf8"
+)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cur.fetchall()
+for row in rows:
+print(row)
+cur.close()
+db.close()
