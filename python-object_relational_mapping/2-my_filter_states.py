2be3793031aed622199d1ff50e68a2f2a74273d2
atanih <torresalva09@gmail.com>
2026-07-05 21:10:51 -0400

    2. Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4])
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC" \
+        .format(sys.argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
