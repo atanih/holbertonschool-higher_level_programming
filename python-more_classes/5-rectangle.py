a7d6e2d49ce6079e1f4c8ff9c59e52c4770d0b60
atanih <torresalva09@gmail.com>
2026-07-30 21:22:22 -0400

    5. Detect instance deletion: add __del__

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Module 5-rectangle.
+
+Defines a class ``Rectangle`` that says goodbye when deleted.
+"""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): the width of the new rectangle.
+            height (int): the height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """int: the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """int: the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle, 0 if a side is 0."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Return the rectangle drawn with the character ``#``."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rows = ["#" * self.__width for _ in range(self.__height)]
+        return "\n".join(rows)
+
+    def __repr__(self):
+        """Return a string able to recreate the rectangle with eval()."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print a message when the rectangle is deleted."""
+        print("Bye rectangle...")
