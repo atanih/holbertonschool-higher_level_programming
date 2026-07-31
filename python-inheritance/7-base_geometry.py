ce83701e4586ecbde693d302d4680f89f81558e7
atanih <torresalva09@gmail.com>
2026-07-30 21:37:38 -0400

    7. Integer validator: cover area, bools, containers and missing args

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a BaseGeometry class."""
+"""Module 7-base_geometry.
+
+Defines a class ``BaseGeometry`` with an unimplemented area and an
+integer validator.
+"""
 
 
 class BaseGeometry:
-    """A base geometry class."""
+    """Represent the base geometry of every shape."""
 
     def area(self):
-        """Raise an Exception with the message area() is not implemented."""
+        """Raise an Exception because area is not implemented yet."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that value is a positive integer."""
+        """Validate that ``value`` is a positive integer.
+
+        Args:
+            name (str): the name of the value, used in the messages.
+            value (int): the value to validate.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
